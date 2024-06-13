8df9e657825e1621d37d6434110011aa35bb087d
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2024-06-13 22:35:39 +0000

    Add files via upload
    Inheritance

---
+++ 1/Program.cs
@@ -0,0 +1,41 @@
+﻿    using System;
+
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            Console.WriteLine("Inheritance and Method Overriding:");
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+            Console.WriteLine();
+        }
+    }
